c6e04bcd4f85f40ffcfc729398e77fc12bc5b00d
Atadais <bog.men98@gmail.com>
2026-07-30 22:14:19 +0200

    Delete backend directory

--- backend/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-function authMiddleware(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || authHeader !== 'Bearer ' + process.env.ADMIN_PASSWORD) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-  next();
-}
-
-module.exports = { authMiddleware };
